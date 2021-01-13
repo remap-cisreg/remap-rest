db0391a59bcf9aaa8389d0117a85d0e4f73724c2
Benoit Ballester <4662116+benoitballester@users.noreply.github.com>
2021-01-13 16:30:21 +0100

    cleaning

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #mongoimport --host mongodb --db test --collection census --type json --file /mongo-seed/census.json --jsonArray
-mongorestore  /Users/benoit/data/db/mongodump/ 
+#mongorestore  /Users/benoit/data/db/mongodump/ 
