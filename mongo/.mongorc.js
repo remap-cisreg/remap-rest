70767b72d88e2d51c51bad942477d5f056f8bb9d
Benoit Ballester <4662116+benoitballester@users.noreply.github.com>
2021-03-29 14:45:48 +0200

    Update .mongorc.js
    better prompt

--- mongo/.mongorc.js
+++ mongo/.mongorc.js
@@ -1,4 +1,20 @@
 host = db.serverStatus().host;
+
 prompt = function() {
-             return db+"@"+host+"$ ";
-         }
+
+    var username = "";
+    var user = db.runCommand({
+            connectionStatus: 1
+        }).authInfo.authenticatedUsers[0];
+
+    if (!!user) {
+        username = user.user;
+    } else {
+        username = "anonymous";
+    }
+
+    var database = db.getName();
+
+    return `[MongoDB] ${username}@${database} > `;
+
+}
